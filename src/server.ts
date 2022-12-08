1fd0a9d9123bf645303e57f404bff9a33bf85dc5
TiagoM13 <thyagomotha2000@gmail.com>
2022-12-08 20:51:34 -0300

    feat: create routes with express

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,19 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([
+    { id: '1', name: 'God of War' }
+  ]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameID = request.params.id;
+
   return response.json([
     { id: '1', name: 'Anúncio 1 ' },
     { id: '2', name: 'Anúncio 2 ' },
@@ -10,6 +22,12 @@ app.get('/ads', (request, response) => {
     { id: '4', name: 'Anúncio 4 ' },
     { id: '4', name: 'Anúncio 5 ' }
   ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adID = request.params.id;
+
+  return response.json([])
 });
 
 app.listen(3333);

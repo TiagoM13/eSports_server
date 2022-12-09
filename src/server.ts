ec9f253884a22bc332bb2395d634caa733b5212a
TiagoM13 <thyagomotha2000@gmail.com>
2022-12-08 22:29:56 -0300

    feat: list games

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/games', (request, response) => {
-  return response.json([
-    { id: '1', name: 'God of War' }
-  ]);
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  });
+
+  return response.json(games);
 });
 
 app.post('/ads', (request, response) => {
